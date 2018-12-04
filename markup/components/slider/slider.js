ab5e0497c1fcd7d6e00192f9e4f72afe25e6a24f
ispitsyn <spitsyn.mwork@gmail.com>
2018-12-04 12:43:53 +0300

    bug fix

--- markup/components/slider/slider.js
+++ markup/components/slider/slider.js
@@ -1,1 +1,9 @@
-
+$(function () {
+   $('.slider__list').slick({
+       infinite: true,
+       slidesToShow: 4,
+       slidesToScroll: 4,
+       dots: true,
+       dotsClass: 'slider__dots'
+   })
+});
