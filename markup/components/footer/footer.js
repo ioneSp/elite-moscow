566a447522e2bf63039e2b9a93138ad8392221c3
krainov <kraino@synergy-lab.ru>
2018-12-10 13:59:17 +0300

    fix

--- markup/components/footer/footer.js
+++ markup/components/footer/footer.js
@@ -1,6 +1,7 @@
 $(document).on('click', '.footer__header', function(e) {
     var $this = $(this);
-    debugger;
+    $('.footer__header').removeClass('active');
+    $(this).addClass('active');
     $this.closest('.footer__column-content').siblings('.footer__column-content').find('.footer__list').removeClass('active');
     $this.next().addClass('active');
 });
