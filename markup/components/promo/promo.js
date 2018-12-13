a2f26748e77fe78a99adb70fc63b409abd5a7bd6
krainov <kraino@synergy-lab.ru>
2018-12-13 15:00:45 +0300

    fix

--- markup/components/promo/promo.js
+++ markup/components/promo/promo.js
@@ -11,23 +11,26 @@ $(".read-more").click(function(e) {
         }
         $(".ISProductBody").toggle(); 
             
-}); 
+});
+
 $(document).ready(function(){
-    var show = true;
-	var countbox = ".promo_type_main";
-	$(window).on("scroll load resize", function(){
-		if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
-		var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
-		var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
-		var w_height = $(window).height();        // Высота окна браузера
-		var d_height = $(document).height();      // Высота всего документа
-		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-		if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
-			$(".spincrement").spincrement({
-				thousandSeparator: "",
-				duration: 1200
-			});
-			show = false;
-		}
-	});
+    if($(".promo_type_main").length) {
+        var show = true;
+        var countbox = ".promo_type_main";
+        $(window).on("scroll load resize", function () {
+            if (!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+            var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+            var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+            var w_height = $(window).height();        // Высота окна браузера
+            var d_height = $(document).height();      // Высота всего документа
+            var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+            if (w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+                $(".spincrement").spincrement({
+                    thousandSeparator: "",
+                    duration: 1200
+                });
+                show = false;
+            }
+        });
+    }
 });
