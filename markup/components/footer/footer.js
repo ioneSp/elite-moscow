fda70a27b0ee6640bb8f680c6db540ee9e2d9304
iones <spitsyn.mwork@gmail.com>
2018-12-13 11:43:34 +0300

    index: edit

--- markup/components/footer/footer.js
+++ markup/components/footer/footer.js
@@ -1,7 +1,12 @@
 $(document).on('click', '.footer__header', function(e) {
     var $this = $(this);
-    $('.footer__header').removeClass('active');
-    $(this).addClass('active');
-    $this.closest('.footer__column-content').siblings('.footer__column-content').find('.footer__list').removeClass('active');
-    $this.next().addClass('active');
+    if(!$this.hasClass('active')) {
+        $('.footer__header').removeClass('active');
+        $this.addClass('active');
+        $this.closest('.footer__column-content').siblings('.footer__column-content').find('.footer__list').removeClass('active');
+        $this.next().addClass('active');
+    } else {
+        $this.removeClass('active');
+        $this.next().removeClass('active');
+    }
 });
